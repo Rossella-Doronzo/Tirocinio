19a1d3b2bc0e175278de214f6503efbcf3b59b07
rossella.doronzo <rossella.doronzo@accenture.com>
2025-04-30 16:05:29 +0200

    Esercizio array oggetti

--- esercizi/array/Studenti.java
+++ esercizi/array/Studenti.java
@@ -2,12 +2,9 @@
 implementare un programma che salvi dei studenti con nome cognome facolta. Si crei un
 array che vada a memorizzare questi studenti, creare 2 metodi: uno che stampi tutti i
 dati degli studenti, uno che stampi solo il nome di tutti gli studenti.
-cominciamo a pensare con una visione di frammentazione logia: non facciamo tutto dentro al main, ma creiamo
-dei METODI dove andiamo a scrivere la logica.
-Quindi nel main chiameremo questi metodi passando come parametro / i le informazioni su cio dobbiamo lavorare.
+non facciamo tutto dentro al main, ma creiamo dei METODI dove andiamo a scrivere la logica.
+Quindi nel main chiameremo questi metodi passando come parametro / i le informazioni
 
-l'esercizio degli oggetti comincia a ragionare proprio ad oggetti, con classi dati e classi logia distinte. poi
-nel main andiamo solo a inserire gli studenti e chiamare i metodi che ci servono.
  */
 
 
